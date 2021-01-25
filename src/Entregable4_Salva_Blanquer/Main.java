c10e33d9eaa87292e337494b0c621a579f88850c
salva <salvablablanquer0@gmail.com>
2021-01-25 18:59:44 +0100

    Navegacion en dos paginas web y busqueda de productos

--- src/Entregable4_Salva_Blanquer/Main.java
+++ src/Entregable4_Salva_Blanquer/Main.java
@@ -7,8 +7,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.Scanner;
 /**
  * @author Salvador
  *
@@ -17,24 +17,51 @@ public class Main {
 	private static WebDriver driver;
 	public static void main(String[] args) {
 
-		String exePath = "C:\\Users\\salva\\Desktop\\de salva\\Programas Camarafp\\Programas Eclipse\\Web_scraping_Salva_Blanquer\\chromedriver.exe";
+		Busqueda1();
+		Busqueda2();
+
+	}
+
+
+public static void Busqueda1(){
+	String Movil;
+	Scanner Teclado = new Scanner(System.in);
+
+	String exePath = "..\\Web_scraping_Salva_Blanquer\\chromedriver.exe";
+	System.setProperty("webdriver.chrome.driver",exePath);
+
+	ChromeOptions options = new ChromeOptions();
+	ChromeOptions chromeOptions = options.addArguments("--start-maximized");
+	driver = new ChromeDriver(options);
+	driver.get("https://www.pccomponentes.com/");
+
+	WebElement element= driver.findElement(By.xpath("//*[@id=\"pcc-search-api--input\"]/div/form/input"));
+
+	System.out.println("Que Smartphone deseas buscar");
+	Movil= Teclado.nextLine();
+	element.sendKeys(Movil);
+	element.submit();
+
+
+}
+
+	public static void Busqueda2(){
+		String Movil;
+		Scanner Teclado = new Scanner(System.in);
+
+		String exePath = "..\\Web_scraping_Salva_Blanquer\\chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver",exePath);
+
 		ChromeOptions options = new ChromeOptions();
 		ChromeOptions chromeOptions = options.addArguments("--start-maximized");
 		driver = new ChromeDriver(options);
-		driver.get("http://www.google.es");
-		WebElement element= driver.findElement(By.name("q"));
-		element.sendKeys("pc componentes");
+		driver.get("https://www.amazon.es");
+
+		WebElement element= driver.findElement(By.xpath("//*[@id=\"twotabsearchtextbox\"]"));
+
+		System.out.println("Que Smartphone deseas buscar");
+		Movil= Teclado.nextLine();
+		element.sendKeys(Movil);
 		element.submit();
-		WebDriverWait waiting= new WebDriverWait(driver,10);
 
-
-
-		WebElement ventanaCookies =  driver.findElement(By.xpath("/html/body/aside/div/button"));
-		if (ventanaCookies !=null){System.out.println("Detectado caja de cookies");
-		ventanaCookies.click();}
-
-	}
-}
-
-
+}}
