e74ba833bae769d2694a080c02bf7ff5e93d88bf
Salva DAM <sblanquer@campuscamarafp.com>
2021-01-28 14:00:56 +0100

    Mejoras añadidas

--- src/Entregable4_Salva_Blanquer/Main.java
+++ src/Entregable4_Salva_Blanquer/Main.java
@@ -1,6 +1,5 @@
-package Entregable4_Salva_Blanquer; /**
- * 
- */
+package Entregable4_Salva_Blanquer;
+
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -16,11 +15,13 @@ import java.util.Scanner;
 public class Main {
 	private static WebDriver driver;
 	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
 
 
 
-
-		Busqueda1();
+	//	Busqueda1();
+		System.out.println("Pulsa enter para la siguiente busqueda");
+		teclado.nextLine();
 		Busqueda2();
 
 	}
@@ -28,8 +29,8 @@ public class Main {
 
 public static void Busqueda1(){
 
-	String Movil;
-	Scanner Teclado = new Scanner(System.in);
+	String movil1;
+	Scanner teclado = new Scanner(System.in);
 
 	String exePath = "..\\Web_scraping_Salva_Blanquer\\chromedriver.exe";
 	System.setProperty("webdriver.chrome.driver",exePath);
@@ -37,33 +38,43 @@ public static void Busqueda1(){
 	ChromeOptions options = new ChromeOptions();
 	ChromeOptions chromeOptions = options.addArguments("--start-maximized");
 	driver = new ChromeDriver(options);
-	driver.get("https://www.pccomponentes.com/");
+	driver.get("https://www.pccomponentes.com/smartphone-moviles");
 
-	/**
-	System.out.println("Pulsa Enter para ir a la seccion de moviles");
-	Teclado.nextLine();
-	WebElement Menu= driver.findElement(By.xpath("\t//*[@id=\"open-main-menu\"]"));
-	Menu.click();
-	WebElement MenuMoviles= driver.findElement(By.xpath("//*[@id=\"GTM-superitem-237\"]/span"));
-	Menu.click();
-	 */
+
 	WebElement BarraDeBusqueda= driver.findElement(By.xpath("//*[@id=\"pcc-search-api--input\"]/div/form/input"));
 
 
 	System.out.println("Que Smartphone deseas buscar");
-	Movil= Teclado.nextLine();
-	BarraDeBusqueda.sendKeys(Movil);
+	movil1= teclado.nextLine();
+	BarraDeBusqueda.sendKeys(movil1);
 	BarraDeBusqueda.submit();
 
-	System.out.println("Pulsa enter para ver precios");
-	Teclado.nextLine();
-	String resultados1 = driver.findElement(By.xpath("//*[@id=\"pcc-search-api--hits\"]/div[2]/ol")).getText();
-	System.out.println(resultados1);
 
-}
+	System.out.println("Enter para selecionar la categoria de moviles");
+	teclado.nextLine();
+	WebElement Categoria= driver.findElement(By.xpath("/html/body/header/div[3]/div[2]/aside/div[3]/div[2]/div/ul/li[1]/div/div"));
+	Categoria.click();
+
+	System.out.println("Pulsa enter para ver precios");
+	teclado.nextLine();
+	System.out.println("---------------------------PC_COMPONENTES-----------------------");
+
+
+
+	for (int i = 1; i < 4 ; i++) {
+
+
+			String modelo = driver.findElement(By.xpath("//*[@id=\"pcc-search-api--hits\"]/div[2]/ol/li["+i+"]")).getText();
+
+
+	System.out.println(modelo);
+		System.out.println("----------------------------------------------------------------");
+}}
+
+;
 
 	public static void Busqueda2(){
-		String Movil;
+		String movil2;
 		Scanner Teclado = new Scanner(System.in);
 
 		String exePath = "..\\Web_scraping_Salva_Blanquer\\chromedriver.exe";
@@ -77,17 +88,20 @@ public static void Busqueda1(){
 		WebElement element= driver.findElement(By.xpath("//*[@id=\"twotabsearchtextbox\"]"));
 
 		System.out.println("Que Smartphone deseas buscar");
-		Movil= Teclado.nextLine();
-		element.sendKeys(Movil);
+		movil2= Teclado.nextLine();
+		element.sendKeys(movil2);
 		element.submit();
 
 		System.out.println(" Pulsa enter para ver precios");
 		Teclado.nextLine();
+		System.out.println("---------------------------AMAZON-----------------------");
 
-		for (int p = 3; p < p; p++) {
+		for (int i = 1; i < 4 ; i++) {
 
-		String resultados2 = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[1]/div[2]/div/span[3]/div[2]/div[3]/div")).getText();
+
+		String resultados2 = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[1]/div[2]/div/span[3]/div[2]/div["+i+"]/div")).getText();
+
 		System.out.println(resultados2);
-
+		System.out.println("----------------------------------------------------------------");
 
 }}}
